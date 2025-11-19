af7f1cbad324c1a3b280cb0a035545e9c7aecb4b
Antonin Décimo <antonin@tarides.com>
2025-11-19 08:35:18 +0100

    Always use __declspec(import) on environ on Windows
    It works with mingw-w64 too.
    
        warning: '__p__environ' redeclared without dllimport attribute: previous dllimport ignored [-Wattributes]
    
    https://gcc.gnu.org/onlinedocs/gcc/Microsoft-Windows-Variable-Attributes.html#index-dllimport-variable-attribute
    
    fixup

--- src/platform/os_ipc_stub.c
+++ src/platform/os_ipc_stub.c
@@ -30,7 +30,7 @@ typedef SSIZE_T ssize_t;
 #include <caml/alloc.h>
 #include <caml/threads.h>
 
-#ifdef _MSC_VER
+#ifdef _WIN32
 extern __declspec(dllimport) char **environ;
 #else
 extern char **environ;
